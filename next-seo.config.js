5da77be34978c6890b4bfcc89113772e1c57db7c
Cole Peterson <rcolepeterson@gmail.com>
2022-03-18 11:09:06 -0700

    seo desc

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,9 +1,12 @@
 export default {
+  title: "The Don't Blink Break",
+  description:
+    "Take a break with Kit Kat. Take a blink break and have a staring contest. See how you stack up in the world’s first ever digital staring contest. Stare at some animals and see who blinks first. Take a blink break and waste some time staring. It’ll be a fun, goofy, silly, time with this international staring contest. Let’s waste some time!",
   openGraph: {
     type: "website",
     locale: "en_IE",
     url: "https://www.url.ie/",
-    site_name: "SiteName",
+    site_name: "The Don't Blink Break",
   },
   twitter: {
     handle: "@handle",
